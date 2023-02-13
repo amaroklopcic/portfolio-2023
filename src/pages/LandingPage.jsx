25b99b4f3d02ee718d6b28fa2bdae032e9491a15
Amar Oklopcic <amar_oklopcic@live.com>
2023-02-13 14:04:10 -0600

    adjust colors and font size on landing page

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -4,17 +4,23 @@ function LandingPage() {
   return (
     <NorthernLightsBackground>
       <div className="landing-header-text-div">
-        <p className="landing-header-text">
+        {/* TODO: need to split the mountains svg image into several components,
+          the moon, so we can make it glow or remove it entirely and just show aurora lights,
+          the stars, so we can add our own and make a couple of them dynamically brighter/dimmer,
+          the background, so when we add our own stars we can lay the mountains on top of them
+        */}
+        {/* TODO: make the image scale with screen size and make it unaffected by zoom */}
+        <p className="landing-header-text" style={{ color: "#C1EBF1" }}>
           Amar Oklopcic
         </p>
-        <p className="landing-header-text" style={{ fontSize: "42px" }}>
+        <p className="landing-header-text" style={{ fontSize: "32px" }}>
           {"Welcome to my portfolio! "}
           <a
             href="#about"
             style={{
               backgroundColor: "transparent",
               borderColor: "transparent",
-              color: "#00218F",
+              color: "#50A1AB",
             }}
           >Click here</a>
           {" to see some of my work :)"}
