af2b536748ce800d68db87ff32c7196e76c27305
Amar Oklopcic <amar_oklopcic@live.com>
2023-02-13 14:03:47 -0600

    add mountains background behind northern lights

--- src/components/NorthernLights/NorthernLights.jsx
+++ src/components/NorthernLights/NorthernLights.jsx
@@ -1,10 +1,18 @@
-import Stars from "./Stars";
-import "./NorthernLights.css";
 import AuroraLights from "./AuroraLights";
+import Mountains from "./Mountains.svg";
+import "./NorthernLights.css";
+import Stars from "./Stars";
 
 function NorthernLights(props) {
     return (
-        <div className="container">
+        <div className="container" style={{ backgroundColor: "#213033" }}>
+            {/* TODO: instead of width: 100%, add some media queries to make the mountains bigger*/}
+            <div style={{
+                width: "100%",
+                height: "100%",
+                background: `#213033 url(${Mountains}) no-repeat bottom center`,
+                backgroundSize: "100% auto",
+            }} />
             <div className="auroral-northern" />
             <Stars />
             <AuroraLights />
