14f0d531a8e2b5bf7bcea3f924aead63c3eb7224
Amar Oklopcic <amar_oklopcic@live.com>
2023-02-15 14:08:52 -0600

    wip: start integrating previous works json data

--- src/pages/PreviousWorks.jsx
+++ src/pages/PreviousWorks.jsx
@@ -1,21 +1,47 @@
 import { Carousel, CarouselSlide } from "../components/Carousel/Carousel";
+import previousWorksData from "../data/previous-works.json";
 
 function PreviousWorksSection() {
-  const previousWork = ["work1", "work2", "work3", "work4"];
-
   return (
     <div
       id="previous-work"
       className="container"
     >
-      <h1 style={{ color: "#C1EBF1", fontSize: "32px" }}>Previous Work</h1>
+      <h1 style={{ color: "#C1EBF1", fontSize: "32px", marginBottom: "3rem" }}>Previous Work</h1>
       <Carousel style={{ width: "800px", height: "500px", margin: "auto auto" }}>
-        {previousWork.map((prevWork, index) => {
+        {previousWorksData.map((data, index) => {
           return (
             <CarouselSlide key={index}>
-              <p>{prevWork}</p>
-              <p>ronald</p>
-              <p>ayo</p>
+              <h1 style={{ marginBottom: "1rem" }}>{data.name}</h1>
+              <div style={{ display: "flex", flexDirection: "row" }}>
+                <div style={{ width: "50%", margin: "0.5rem" }}>
+                  <img
+                    src={data["image-link"] || "https://developers.elementor.com/docs/assets/img/elementor-placeholder-image.png"}
+                    alt="placeholder"
+                    style={{
+                      width: "100%"
+                    }}
+                  />
+                </div>
+                <div style={{ width: "50%", margin: "0.5rem" }}>
+                  <p>{data["date-developed"]}</p>
+                  <p>{data.description}</p>
+                </div>
+              </div>
+              <div style={{ textAlign: "left", marginLeft: "1rem" }}>
+                <h3>Tech Used</h3>
+                {data["tech-used"].map((techName, index) => {
+                  return <span
+                    key={index}
+                    style={{
+                      background: "#68B684",
+                      padding: "0.25rem 0.75rem",
+                      borderRadius: "6px",
+                      margin: "4px"
+                    }}
+                  >{techName}</span>
+                })}
+              </div>
             </CarouselSlide>
           )
         })}
