8ad58aef233e333fddd258485e2adeed40717098
Amar Oklopcic <amar_oklopcic@live.com>
2023-02-15 14:30:21 -0600

    add more text on about me page

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -16,19 +16,39 @@ function AboutSection() {
             <br />
             I love software and making things.
             My passion for software engineering came from a very early age, when I
-            was about 8 or so and started playing a game called
-            <a href="https://en.wikipedia.org/wiki/LittleBigPlanet" style={{ color: "#7EBDC4" }}> "Little Big Planet" </a>
-            that lets you make various machines, levels, and other creations using
+            was about 8 or so and started playing a game called{" "}
+            <a
+              href="https://en.wikipedia.org/wiki/LittleBigPlanet"
+              style={{ color: "#7EBDC4" }}
+            >
+              "Little Big Planet"
+            </a>
+            {" "}that lets you make various machines, levels, and other creations using
             a variety of in-game tools including microchips, logic gates, and other
             things. The creativity that the game allowed led me to spend hundreds
             of hours making various creations and made an imprint on my young mind.
             I still have the same joy, wonder, and love for engineering and
             building things today.
+            <br />
+            <br />
+            I started tinkering with programming when I was about 15 or so in high school,
+            and started really taking it more seriously when I was 16. My first piece of software
+            that I released into the world was an addon for video game called "Garry's Mod" when
+            I was just 16. I was really proud of it since it involved a lot of vector mathematics
+            which was difficult for me at that age but I fell in love with programming because of
+            how rewarding it was when I got it working. Other people loved it as well, it got almost
+            17,000 downloads and was rated 5 out of 5 stars on the{" "}
+            <a
+              href="https://steamcommunity.com/sharedfiles/filedetails/?id=934097023"
+              style={{ color: "#7EBDC4" }}
+            >
+              Steam workshop
+            </a>.
           </p>
           <div style={{ margin: "auto 1rem" }}>
             <img
               src={SackboyPNG}
-              alt=""
+              alt="little big planet sackboy"
             />
           </div>
         </div>
