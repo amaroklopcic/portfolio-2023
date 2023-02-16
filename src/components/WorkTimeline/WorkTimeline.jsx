7549c7ffa4a6fd028304c9eac58e13d2697946d3
Amar Oklopcic <amar_oklopcic@live.com>
2023-02-15 21:46:04 -0600

    swapped previous works carousel for a timeline

---
+++ src/components/WorkTimeline/WorkTimeline.jsx
@@ -0,0 +1,94 @@
+import { Children, cloneElement } from "react";
+import "./WorkTimeline.css";
+
+function WorkTimeline(props) {
+  // console.log(props.children);
+
+  return (
+    <div className="timeline-container">
+      {Children.map(props.children, (child, index) => {
+        return (
+          <div>
+            {cloneElement(child)}
+            {index + 1 !== Children.count(props.children) ? <hr /> : <></>}
+          </div>
+        )
+      })}
+    </div>
+  )
+}
+
+function TimelineCategory(props) {
+  // console.log(props.name);
+  // console.log(props.timeWorked);
+  // console.log(props.icon);
+  // console.log(props.iconAlt);
+  // console.log(props.link);
+
+  return (
+    <div>
+      {/* category header section (icon + category/company name) */}
+      <div style={{ display: "flex", marginTop: "1rem" }}>
+        <img
+          src={props.icon}
+          alt={props.iconAlt}
+          style={{ display: "block", width: "48px", height: "48px", border: "none" }}
+        />
+        <div className="timeline-category-item-container">
+          <b className="timeline-no-space timeline-title-text">{props.name}</b>
+          <p className="timeline-no-space">{props.timeWorked}</p>
+        </div>
+      </div>
+
+      {Children.map(props.children, (child, index) => {
+        return (
+          <div style={{ marginLeft: "1.3rem", marginBottom: "1rem", marginTop: "1rem" }}>
+            {/* different category items (different job positions at the company) */}
+            <div
+              style={{ display: "flex" }}
+            >
+              {/* div containing left side dots/lines */}
+              <div style={{ marginRight: "1rem" }}>
+                <div className="timeline-circle" />
+                {index + 1 !== (props.children || []).length
+                  ? <div className="timeline-vertical-line"></div>
+                  : <></>
+                }
+              </div>
+              {/* TimelineCategoryItem */}
+              {cloneElement(child)}
+            </div>
+          </div>
+        )
+      })}
+    </div>
+  )
+}
+
+function TimelineCategoryItem(props) {
+  // console.log(props.title);
+  // console.log(props.positionType);
+  // console.log(props.timeWorked);
+  // console.log(props.location);
+  // console.log(props.description);
+
+  return (
+    <div className="timeline-category-item-container">
+      <b className="timeline-no-space">{props.title}</b>
+      <p className="timeline-no-space">{props.positionType}</p>
+      <p className="timeline-no-space timeline-text-grey">{props.timeWorked}</p>
+      <p className="timeline-no-space timeline-text-grey">{props.location}</p>
+      <br />
+      <p className="timeline-no-space">
+        <b style={{ fontSize: "14px" }}>Skills: </b>
+        {(props.skills || []).map((skill, index) => {
+          return <span>{skill}{index + 1 !== props.skills.length ? " · " : <></>}</span>
+        })}
+      </p>
+    </div>
+  )
+}
+
+
+export default WorkTimeline;
+export { TimelineCategory, TimelineCategoryItem };
--- src/pages/PreviousWorks.jsx
+++ src/pages/PreviousWorks.jsx
@@ -1,57 +1,53 @@
-import { Carousel, CarouselSlide } from "../components/Carousel/Carousel";
-import previousWorksData from "../data/previous-works.json";
+import WorkTimeline, { TimelineCategory, TimelineCategoryItem } from "../components/WorkTimeline/WorkTimeline";
+import previousWorksTimelineData from "../data/previous-works-timeline.json";
 
 function PreviousWorksSection() {
+  // TODO: change company icons pulled from external sources to be pulled in from our own backend
   return (
     <div
       id="previous-work"
-      className="container"
+      className="relative-position"
     >
-      <h1 style={{ color: "#C1EBF1", fontSize: "32px", marginBottom: "3rem" }}>Previous Work</h1>
-      <Carousel
-        style={{ width: "800px", height: "500px", margin: "auto auto" }}
-        pageIds={previousWorksData.map((prevWork) => prevWork.id)}
-      >
-        {previousWorksData.map((data, index) => {
-          return (
-            <CarouselSlide key={index}>
-              <h1 style={{ marginBottom: "1rem" }}>{data.name}</h1>
-              <div style={{ display: "flex", flexDirection: "row" }}>
-                <div style={{ width: "50%", margin: "0.5rem" }}>
-                  <img
-                    src={data["image-link"] || "https://developers.elementor.com/docs/assets/img/elementor-placeholder-image.png"}
-                    alt="placeholder"
-                    style={{
-                      width: "100%",
-                      borderRadius: "6px"
-                    }}
-                  />
-                </div>
-                <div style={{ width: "50%", margin: "0.5rem" }}>
-                  <p>{data["date-developed"]}</p>
-                  <p>{data.description}</p>
-                </div>
-              </div>
-              <div style={{ textAlign: "left", marginLeft: "1rem" }}>
-                <h3 style={{ marginBottom: "0.5rem" }}>Tech Used</h3>
-                <div style={{ display: "flex", "flexWrap": "wrap" }}>
-                  {data["tech-used"].map((techName, index) => {
-                    return <span
-                      key={index}
-                      style={{
-                        background: "#68B684",
-                        padding: "0.25rem 0.75rem",
-                        borderRadius: "6px",
-                        margin: "4px"
-                      }}
-                    >{techName}</span>
+      <a href="#previous-work" className="link">
+        <h1 style={{ color: "#C1EBF1", fontSize: "32px", marginBottom: "3rem", marginTop: "6rem" }}>
+          Previous Work
+        </h1>
+      </a>
+
+      <div>
+        <div
+          style={{ width: "500px", margin: "auto auto" }}
+        >
+          <WorkTimeline>
+            {previousWorksTimelineData.map((previousWork, index) => {
+              return (
+                <TimelineCategory
+                  key={index}
+                  name={previousWork.companyName}
+                  timeWorked={previousWork.timeAtCompany}
+                  icon={previousWork.icon}
+                  iconAlt={previousWork.iconAlt}
+                  link={previousWork.companyWebsiteLink}
+                >
+                  {previousWork.positions.map((previousPosition, index) => {
+                    return (
+                      <TimelineCategoryItem
+                        key={index}
+                        title={previousPosition.title}
+                        positionType={previousPosition.positionType}
+                        timeWorked={previousPosition.positionTimeframe}
+                        location={previousPosition.location}
+                        description={previousPosition.description}
+                        skills={previousPosition.skills}
+                      />
+                    )
                   })}
-                </div>
-              </div>
-            </CarouselSlide>
-          )
-        })}
-      </Carousel>
+                </TimelineCategory>
+              )
+            })}
+          </WorkTimeline>
+        </div>
+      </div>
     </div>
   );
 }
