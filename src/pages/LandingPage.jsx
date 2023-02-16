9ddbe55e0d169c079b4a1a80f646110598db40d2
Amar Oklopcic <amar_oklopcic@live.com>
2023-02-15 22:37:13 -0600

    removed old TODOs on LandingPage component

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -2,12 +2,6 @@ function LandingPage() {
   return (
     <div className="container">
       <div className="landing-header-text-div">
-        {/* TODO: need to split the mountains svg image into several components,
-          the moon, so we can make it glow or remove it entirely and just show aurora lights,
-          the stars, so we can add our own and make a couple of them dynamically brighter/dimmer,
-          the background, so when we add our own stars we can lay the mountains on top of them
-        */}
-        {/* TODO: make the image scale with screen size and make it unaffected by zoom */}
         <p className="landing-header-text" style={{ color: "#C1EBF1" }}>
           Amar Oklopcic
         </p>
